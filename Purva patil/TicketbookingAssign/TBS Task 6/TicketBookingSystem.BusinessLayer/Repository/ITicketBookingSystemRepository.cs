e31f533edd064260ccd41dbf5394bc40629bbc2c
Purva Patil <89764960+Patilpurva17@users.noreply.github.com>
2024-10-10 22:55:56 +0530

    Add files via upload

---
+++ Purva patil/TicketbookingAssign/TBS Task 6/TicketBookingSystem.BusinessLayer/Repository/ITicketBookingSystemRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TicketBookingSystem.Entity;
+
+namespace TicketBookingSystem.BusinessLayer.Repository
+{
+    internal interface ITicketBookingSystemRepository
+    {
+        Event createEvent(String eventName, DateTime eventDate, TimeSpan eventTime, String venueName, int totalSeats, int availableSeats, Decimal ticketPrice, EventType type, params object[] additionalInfo);
+        void DisplayEventDetails(Event eventObj);
+        void BookTickets(int numTickets, Event eventObj);
+        void CancelTickets(int numTickets, Event eventObj);
+
+    }
+}
