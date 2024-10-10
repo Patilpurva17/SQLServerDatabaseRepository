a4f6646ded72fe66f6f4410593cb642bed66bb94
Purva Patil <89764960+Patilpurva17@users.noreply.github.com>
2024-10-10 22:53:48 +0530

    Add files via upload

---
+++ Purva patil/TicketbookingAssign/TBS Task 5/TicketBookingSystem.BusinessLayer/Services/ITicketBookingSystemServices.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using TicketBookingSystem.Entity;
+using static TicketBookingSystem.Entity.Event;
+
+namespace TicketBookingSystem.BusinessLayer.Services
+{
+    internal interface ITicketBookingSystemServices
+    {
+        Event createEvent(String eventName, DateTime eventDate, TimeSpan eventTime, String venueName, int totalSeats, int availableSeats, Decimal ticketPrice, EventType type, params object[] additionalInfo);
+        void DisplayEventDetails(Event eventObj);
+        void BookTickets(int numTickets, Event eventObj);
+        void CancelTickets(int numTickets, Event eventObj);
+    }
+}
---
+++ Purva patil/TicketbookingAssign/TBS Task 5/TicketBookingSystem.BusinessLayer/Services/TicketBookingSystemServices.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using TicketBookingSystem.BusinessLayer.Repository;
+using TicketBookingSystem.Entity;
+using static TicketBookingSystem.Entity.Event;
+
+namespace TicketBookingSystem.BusinessLayer.Services
+{
+    public class TicketBookingSystemServices : ITicketBookingSystemServices
+    {
+        private readonly TicketBookingSystemRepository repository;
+
+        public TicketBookingSystemServices()
+        {
+            repository = new TicketBookingSystemRepository();
+        }
+
+        public Event createEvent(String eventName, DateTime eventDate, TimeSpan eventTime, String venueName, int totalSeats, int availableSeats, Decimal ticketPrice, EventType type, params object[] additionalInfo)
+        {
+            return repository.createEvent(eventName, eventDate, eventTime, venueName, totalSeats, availableSeats, ticketPrice, type, additionalInfo);
+        }
+
+        public void DisplayEventDetails(Event eventObj)
+        {
+            repository.DisplayEventDetails(eventObj);
+        }
+
+        public void BookTickets(int numTickets, Event eventObj)
+        {
+            repository.BookTickets(numTickets, eventObj);
+        }
+
+        public void CancelTickets(int numTickets, Event eventObj)
+        {
+            repository.CancelTickets(numTickets, eventObj);
+        }
+    }
+}
